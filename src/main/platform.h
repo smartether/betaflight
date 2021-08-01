71cc0ea9f9ce1579e649c48ec814f42eb714ad58
Zhengwei Qian <qianzhengwei@naocy.com>
2021-08-01 11:16:49 +0800

    1,raspberry pi target config.

--- src/main/platform.h
+++ src/main/platform.h
@@ -115,6 +115,18 @@
 #define STM32F1
 #endif
 
+
+#elif defined(BCM)
+
+// Chip Unique ID on F103
+#define U_ID_0 (*(uint32_t*)0x1FFFF7E8)
+#define U_ID_1 (*(uint32_t*)0x1FFFF7EC)
+#define U_ID_2 (*(uint32_t*)0x1FFFF7F0)
+
+#ifndef PI_ZERO
+#define PI_ZERO
+#endif
+
 #elif defined(SIMULATOR_BUILD)
 
 // Nop
